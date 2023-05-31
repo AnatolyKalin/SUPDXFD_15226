b42bbc8e840f8f6b43426f66415b93d36be0860f
AnatolyKalin <anatoly.kalin@gmail.com>
2023-05-31 16:40:03 +0300

    Call\Put strike prices

--- main.c
+++ main.c
@@ -128,26 +128,6 @@ const char *symbol_type_to_string(symbol_type_t symbol_type) {
     }
 }
 
-/*
- * https://kb.dxfeed.com/en/data-model/instrument-profile-format.html#list-of-fields-60638
- * https://en.wikipedia.org/wiki/ISO_10962
- */
-int is_call_option(const char *cfi) {
-    if (cfi == NULL || strlen(cfi) < 2 || cfi[0] != 'O') {
-        return 0;
-    }
-
-    return cfi[1] == 'C';
-}
-
-int is_put_option(const char *cfi) {
-    if (cfi == NULL || strlen(cfi) < 2 || cfi[0] != 'O') {
-        return 0;
-    }
-
-    return cfi[1] == 'P';
-}
-
 #define CREATE_BUFFER(SIZE) const size_t BUFFER_SIZE = SIZE; char buffer[BUFFER_SIZE]; memset(buffer, 0, BUFFER_SIZE * sizeof(char))
 
 void get_symbols_by_type(const char *base_url, const char *user, const char *password, symbol_type_t symbol_type,
@@ -199,9 +179,35 @@ void get_expire_date_of_option(const char *base_url, const char *user, const cha
     perform_get(buffer, user, password, response);
 }
 
+void get_call_or_put_strike_price_on_specific_expire_date_of_option(const char *base_url, const char *user,
+                                                                    const char *password, const char *symbol,
+                                                                    const char *expire_date, int is_call,
+                                                                    response_t *response) {
+    if (symbol == NULL || expire_date == NULL) {
+        return;
+    }
+
+    CREATE_BUFFER(2048);
+
+    /*
+     * https://kb.dxfeed.com/en/data-model/instrument-profile-format.html#list-of-fields-60638
+     * https://en.wikipedia.org/wiki/ISO_10962
+     *
+     * Call's  CFI starts with `OC*`
+     */
+    char *cfi = is_call ? "OC*" : "OP*";
+    snprintf(buffer, BUFFER_SIZE,
+             "%s?TYPE=%s&fields=UNDERLYING,PRODUCT,CFI,STRIKE,EXPIRATION&UNDERLYING=%s&CFI=%s&EXPIRATION=%s", base_url,
+             symbol_type_to_string(ST_OPTION),
+             symbol, cfi, expire_date);
+
+    perform_get(buffer, user, password, response);
+}
 
 
 int main() {
+    /* https://tools.dxfeed.com/ipf?help */
+
     const char *ipf_url = "https://tools.dxfeed.com/ipf";
     const char *user = "demo";
     const char *password = "demo";
@@ -211,55 +217,94 @@ int main() {
 
     /* 1) */
     {
-        response_t get_symbols_by_type_response = {0};
-        get_symbols_by_type(ipf_url, user, password, ST_STOCK, &get_symbols_by_type_response);
+        response_t response = {0};
+        get_symbols_by_type(ipf_url, user, password, ST_STOCK, &response);
 
-        if (get_symbols_by_type_response.data != NULL) {
-            printf("\n\nALL STOCK SYMBOLS\n----------\n%s\n----------\n", get_symbols_by_type_response.data);
+        if (response.data != NULL) {
+            printf("\n\nALL STOCK SYMBOLS\n----------\n%s\n----------\n", response.data);
 
-            free(get_symbols_by_type_response.data);
+            free(response.data);
         }
     }
 
     /* 2) */
     {
-        response_t get_symbols_by_type_starting_with_symbol_name_response = {0};
+        response_t response = {0};
         char *symbol_name_start = "A";
 
         get_symbols_by_type_starting_with_symbol_name(ipf_url, user, password, ST_STOCK, symbol_name_start,
-                                                      &get_symbols_by_type_starting_with_symbol_name_response);
+                                                      &response);
 
-        if (get_symbols_by_type_starting_with_symbol_name_response.data != NULL) {
+        if (response.data != NULL) {
             printf("\n\nALL STOCK SYMBOLS STARTING WITH \"%s\"\n----------\n%s\n----------\n",
-                   symbol_name_start, get_symbols_by_type_starting_with_symbol_name_response.data);
+                   symbol_name_start, response.data);
 
-            free(get_symbols_by_type_starting_with_symbol_name_response.data);
+            free(response.data);
         }
     }
 
     /* 3) */
 
     {
-        response_t get_expire_date_of_option_response = {0};
+        response_t response = {0};
 
         /* Retrieve stocks (https://tools.dxfeed.com/ipf?TYPE=STOCK), parse symbols and put here ---v */
         char *symbol_name = "AAPL,IBM,META";
 
         get_expire_date_of_option(ipf_url, user, password, symbol_name,
-                                                      &get_expire_date_of_option_response);
+                                  &response);
 
-        if (get_expire_date_of_option_response.data != NULL) {
+        if (response.data != NULL) {
             printf("\n\nALL EXPIRE DATES OF STOCK OPTIONS \"%s\"\n----------\n%s\n----------\n",
-                   symbol_name, get_expire_date_of_option_response.data);
+                   symbol_name, response.data);
 
-            free(get_expire_date_of_option_response.data);
+            free(response.data);
         }
     }
 
     /* 4) */
 
     {
+        /* CALL STRIKE PRICES */
+        {
+            response_t response = {0};
 
+            /* Retrieve stocks (https://tools.dxfeed.com/ipf?TYPE=STOCK), parse symbols and put here ---v */
+            char *symbol_name = "AAPL,IBM,META";
+            char *expire_date = "2023-06-23";
+
+            get_call_or_put_strike_price_on_specific_expire_date_of_option(
+                    ipf_url, user, password, symbol_name,
+                    expire_date, 1,
+                    &response);
+
+            if (response.data != NULL) {
+                printf("\n\nCALL STRIKE PRICES ON %s OF STOCK OPTIONS \"%s\"\n----------\n%s\n----------\n",
+                       expire_date, symbol_name, response.data);
+
+                free(response.data);
+            }
+        }
+        /* PUT STRIKE PRICES */
+        {
+            response_t response = {0};
+
+            /* Retrieve stocks (https://tools.dxfeed.com/ipf?TYPE=STOCK), parse symbols and put here ---v */
+            char *symbol_name = "AAPL,IBM,META";
+            char *expire_date = "2023-06-23";
+
+            get_call_or_put_strike_price_on_specific_expire_date_of_option(
+                    ipf_url, user, password, symbol_name,
+                    expire_date, 0,
+                    &response);
+
+            if (response.data != NULL) {
+                printf("\n\nPUT STRIKE PRICES ON %s OF STOCK OPTIONS \"%s\"\n----------\n%s\n----------\n",
+                       expire_date, symbol_name, response.data);
+
+                free(response.data);
+            }
+        }
     }
 
     curl_global_cleanup();
